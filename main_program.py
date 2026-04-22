67d49bb104aeb870ffc5455988c6bd27be5df1fe
conradjosesalayog <radradsalayog@gmail.com>
2026-04-22 18:08:03 +0800

    Fixed indentation for number_separator.py. Also added main function in the main program. Using variables, we embedded the class to the actual number text file so it can work. We applied the separate_odd_even function to the separator variable

--- main_program.py
+++ main_program.py
@@ -1,1 +1,5 @@
 from number_separator import NumberSeparator
+
+def main():
+    separator = NumberSeparator("numbers.txt")
+    separator.separate_odd_even()
--- number_separator.py
+++ number_separator.py
@@ -10,19 +10,19 @@ class NumberSeparator:
             with open(self.numbers_file, "r") as file:
                 numbers = file.read().split()
 
-            for number in numbers:
-                number = int(number)
-                if number % 2 == 0:
-                   even_numbers.append(number)
-                else:
-                   odd_numbers.append(number)
+                for number in numbers:
+                    number = int(number)
+                    if number % 2 == 0:
+                       even_numbers.append(number)
+                    else:
+                       odd_numbers.append(number)
         except:
             print("Error")
 
-        with open("/.even_numbers.txt", "w") as even_numbers_file:
+        with open("even_numbers.txt", "w") as even_numbers_file:
             for number in even_numbers:
                 even_numbers_file.write(str(number))
 
-        with open("/.odd_numbers.txt", "w") as odd_numbers_file:
+        with open("odd_numbers.txt", "w") as odd_numbers_file:
             for number in odd_numbers:
                 odd_numbers_file.write(str(number))
