d07b3a8344d4091a45e9ffa780f007ab26172faa
conradjosesalayog <radradsalayog@gmail.com>
2026-04-28 01:39:40 +0800

    Added function for the reading of the file student.txt with the permission r. Also used for loop in file to split the name and the gwa of a student

--- student_gwa.py
+++ student_gwa.py
@@ -1,9 +1,16 @@
 class StudentGwa:
+    '''Initial class for students and their GWAs'''
     def __init__(self, name, gwa):
         self.name = name
         self.gwa = gwa
 
 class StudentConfig:
+    '''Main class for the configuration of the students and their GWAs'''
     def __init__(self):
         self.file_name = "students.txt"
         self.students = []
+
+    def read_students(self):
+        with open(self.file_name, "r") as file:
+            for line in file:
+                name, gwa = line.split(",")
