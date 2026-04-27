946647c66f8f382a95bf90fbb639592d09cec28a
conradjosesalayog <radradsalayog@gmail.com>
2026-04-28 01:29:54 +0800

    Added class StudentConfig so that we can embed class StudentGwa to it later on. We inserted our students.txt file to the class which is the file with the names of the students and their gwas. Added self.students as an empty list

--- student_gwa.py
+++ student_gwa.py
@@ -2,3 +2,8 @@ class StudentGwa:
     def __init__(self, name, gwa):
         self.name = name
         self.gwa = gwa
+
+class StudentConfig:
+    def __init__(self):
+        self.file_name = "students.txt"
+        self.students = []
