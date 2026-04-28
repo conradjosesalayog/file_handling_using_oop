d2bac7196e1cb1b147d0094c6b2eb55a30a69aeb
conradjosesalayog <radradsalayog@gmail.com>
2026-04-29 01:03:11 +0800

    Added function process and this is where we start using methods like reading and writing a file

--- integer_processor_file.py
+++ integer_processor_file.py
@@ -2,4 +2,7 @@ class IntegerProcessor:
     def __init__(self):
         self.integer_file = "integers.txt"
 
-    
+    def process(self):
+        with open(self.integer_file, "r") as number_file, \
+
+
