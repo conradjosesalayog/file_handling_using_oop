97833e2d088279ac86c03cbd80e99daf606bdd8e
conradjosesalayog <radradsalayog@gmail.com>
2026-04-29 01:56:08 +0800

    Added print function so the user will be notified when the txt files appear

--- calculating_the_square_of_even_numbers_and_cube_of_odd_numbers.py
+++ calculating_the_square_of_even_numbers_and_cube_of_odd_numbers.py
@@ -3,3 +3,6 @@ from integer_processor_file import IntegerProcessor
 def main():
     processor = IntegerProcessor()
     processor.process()
+    print("Check double.txt and triple.txt to see values of the updated integers")
+
+    
